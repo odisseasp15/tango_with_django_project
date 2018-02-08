46e916f4090f4018065f82673129a4a9a489b0d2
Odysseas Polycarpou <2210049P@student.gla.ac.uk>
2018-02-08 15:20:11 +0000

    End of Chapter 8 (9 & 10 w/o Exercises)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator','OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -104,11 +104,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+LOGIN_URL = '/rango/login/'
+
+
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
+SESSION_COOKIE_AGE = 1209600
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
